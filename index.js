13288acd86e4acffc3995c052fbaf8f37d560b35
Zakaria5156 <zakaria.blaouali@gmail.com>
2021-01-28 15:56:04 +0100

    Initial setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 app.get('/', (req, res) => {
-    res.send('Welcome to learn backend with express!')
+    res.send('Welcome to learn backend with express')
 });
 app.listen(8000, () => {
     console.log('Example app listening on port 8000!')
