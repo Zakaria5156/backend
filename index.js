18739ee966d7b5bd9828177ec8a964def70c31ff
Zakaria5156 <zakaria.blaouali@gmail.com>
2021-01-29 14:07:14 +0100

    add new order route

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('express')
+const express = require('express'),
+    bodyParser = require('body-parser');
 const app = express();
+app.use(bodyParser.json());
 
 
 const MongoClient = require('mongodb').MongoClient;
+//Connection string from mongodb
 const uri = "mongodb+srv://coursework:Coursework2@cluster0.vaitt.mongodb.net/test?retryWrites=true&w=majority";
 
 //connecting to MongoDB Atlas using Node JS driver
@@ -12,21 +15,21 @@ MongoClient.connect(uri, {
     .then(client => {
         //Select the database and the collection
         const db = client.db('coursework2')
-        const lessonsCollection = db.collection('lessons')
+        const lessonsCollection = db.collection('lessons');
+        const orderCollection = db.collection("orders");
+
+        //Defining Base route
+        app.get('/', (req, res) => {
+            res.json({
+                'success': true,
+            })
+        });
 
         //Defining the route to retreive the lessons from the collection
-        app.get('/', (req, res) => {
-            const cursor = lessonsCollection.find().toArray()
-                .then(lessons => {
-                    res.json({
-                        'success': true,
-                        'data': lessons
-                    })
-                })
-        });
         app.get('/lessons', (req, res) => {
             const cursor = lessonsCollection.find().toArray()
                 .then(lessons => {
+                    //return the lessons data as response
                     res.json({
                         'success': true,
                         'data': lessons
@@ -34,8 +37,18 @@ MongoClient.connect(uri, {
                 })
         });
 
-        app.listen(process.env.PORT || 3000, () => {
-            console.log('Example app listening on port 8000!')
+        //Defining route to post order
+        app.post('/order', (req, res) => {
+            //Save a new order
+            const result = orderCollection.insertOne(req.body);
+            res.json({
+                'success': true
+            });
+        });
+
+        //Start the server
+        app.listen( process.env.PORT || 8000, () => {
+            console.log('Example app listening on port ' + process.env.PORT)
         });
 
     })
