698c1887e75bf75cbd530f9362169f63047c9948
Zakaria5156 <zakaria.blaouali@gmail.com>
2021-01-29 08:24:13 +0100

    Connection and lessons route

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const express = require('express')
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Welcome to learn backend with express')
-});
-app.listen(8000, () => {
-    console.log('Example app listening on port 8000!')
-});
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://coursework:Coursework2@cluster0.vaitt.mongodb.net/test?retryWrites=true&w=majority";
+
+//connecting to MongoDB Atlas using Node JS driver
+MongoClient.connect(uri, {
+        useUnifiedTopology: true
+    })
+    .then(client => {
+        //Select the database and the collection
+        const db = client.db('coursework2')
+        const lessonsCollection = db.collection('lessons')
+
+        //Defining the route to retreive the lessons from the collection
+        app.get('/lessons', (req, res) => {
+            const cursor = lessonsCollection.find().toArray()
+                .then(lessons => {
+                    res.json({
+                        'success': true,
+                        'data': lessons
+                    })
+                })
+        });
+
+        app.listen(8000, () => {
+            console.log('Example app listening on port 8000!')
+        });
+
+    })
+    .catch(error => console.error(error))
